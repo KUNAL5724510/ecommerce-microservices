22d2a7af9459a2cb9cb5ee1d05b25ac6906cd5b4
kunal5724510 <kunal.sh.dev@gmail.com>
2025-08-27 14:03:55 +0530

    updated US

--- userservice/src/main/java/com/ecommerce/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/ecommerce/userservice/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.ecommerce.user_service;
+package com.ecommerce.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
 }
--- userservice/src/test/java/com/ecommerce/userservice/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/ecommerce/userservice/UserserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecommerce.user_service;
+package com.ecommerce.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class UserserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
